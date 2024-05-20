2d92f5c14b3997ef1663bfc69d7721d51cd216c9
theekshanahansa <theekshanahansa@gmail.com>
2024-05-20 13:13:10 +0530

    Update faculty.py
    Create module class to add modules in faculty in faculty.py using class-modifier-02 branch

--- faculty.py
+++ faculty.py
@@ -1,15 +1,35 @@
+
+
+class FacultyModule:
+    def __init__(self, module_name, module_code):
+        self.module_name = module_name
+        self.module_code = module_code
+
+    def display_details(self):
+        print(f"Module Name: {self.module_name}")
+        print(f"Module Code: {self.module_code}")
+        print("-" * 20)
+
+
 class FacultyMember:
     def __init__(self, name, age, position, department):
         self.name = name
         self.age = age
         self.position = position
         self.department = department
+        self.modules = []
+
+    def add_module(self, module):
+        self.modules.append(module)
 
     def display_details(self):
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Position: {self.position}")
         print(f"Department: {self.department}")
+        print("Modules:")
+        for module in self.modules:
+            module.display_details()
         print("-" * 20)
 
 
