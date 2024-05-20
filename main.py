3718fb6d3610ea844e64fc0c28d4ccee54d164fb
theekshanahansa <theekshanahansa@gmail.com>
2024-05-20 12:55:39 +0530

    Update main.py
    Add new lectures from main.py in main-modifier branch

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from faculty import FacultyMember, FacultyLecture
 
 
 def main():
-    # Create an instance of FacultyLecture
+
     faculty_lecture = FacultyLecture()
 
-    # Create some FacultyMember instances
-    member1 = FacultyMember("Dr. John Doe", 45, "Professor", "Marketing")
-    member2 = FacultyMember("Dr. Jane Smith", 38, "Associate Professor", "Finance")
-    member3 = FacultyMember("Dr. Emily Johnson", 50, "Lecturer", "Management")
+    # instances
+    member1 = FacultyMember("Mr.Supun ", 35, "lecture", "Decision Science")
+    member2 = FacultyMember("Dr.Amali", 40, "Department Head", "Management of technology")
+    member3 = FacultyMember("Dr.Venura ", 50, "Lecture", "Decision science")
 
     # Add members to the faculty lecture
     faculty_lecture.add_member(member1)
